48d81f6a0f0e034a3f0ba30aadcb69fb066c4984
Александр <sasha.krasilnikov@gmail.com>
2024-05-26 13:07:48 +0300

    Add files via upload

---
+++ FigureLibrary.Tests/CircleTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FigureLibrary.Tests
+{
+    [TestClass]
+    public class CircleTests
+    {
+        [TestMethod]
+        public void Square_10_Return314_16()
+        {
+            Circle circle = new("Круг", 10);
+            double expected = 314.16;
+
+            double result = circle.CalcSquare();
+
+            Assert.AreEqual(expected, result,"Площадь должна была быть равна 314,16.");
+        }
+
+        [TestMethod]
+        public void Square_NotExist()
+        {
+            Circle circle = new("Круг", -5);
+
+            Assert.ThrowsException<Exception>(() => circle.CalcSquare());
+        }
+    }
+}
---
+++ FigureLibrary.Tests/TriangleTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FigureLibrary.Tests
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void IsRectangularTriangle_ReturnTrue()
+        {
+            Triangle triangle = new("Треугольник", 3, 4, 5);
+
+            bool result = triangle.IsRectangularTriangle();
+
+            Assert.IsTrue(result, "Треугольник должен быть прямоугольным.");
+        }
+
+        [TestMethod]
+        public void IsRectangularTriangle_ReturnFalse()
+        {
+            Triangle triangle = new("Треугольник", 10, 10, 6);
+
+            bool result = triangle.IsRectangularTriangle();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Square_10and10and6_Return95_53()
+        {
+            Triangle triangle = new("Треугольник", 10, 10, 6);
+
+            double expected = 95.53;
+            double result = triangle.CalcSquare();
+            Assert.AreEqual(expected, result, "Площадь треугольника должна была быть равна 95,63.");
+        }
+    }
+}
